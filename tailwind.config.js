5daccad359ddbfb6e5b95fd914f9d6a127348af3
javiervargas02 <javier.v.021204@gmail.com>
2024-01-27 14:50:46 -0600

    Add theme config to use custom fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./*.html'],
+  content: ["./*.html"],
   theme: {
     extend: {},
+    fontFamily: {
+      sans: ["Mulish", "sans-serif"],
+      mono: ["Rokkitt", "monospace"],
+    },
   },
   plugins: [],
 };
